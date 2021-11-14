e8419d40ea76636f56e5def37ab6efb7aff33d5b
bigheadOOOO <90515693+bigheadOOOO@users.noreply.github.com>
2021-11-14 19:54:36 +0800

    Add files via upload

--- miniprogram/miniprogram/app.js
+++ miniprogram/miniprogram/app.js
@@ -6,9 +6,10 @@ App({
   globalData:{
     username:"USER",
     userimage:"https://s3.bmp.ovh/imgs/2021/10/77a830cbddfff9a8.png",
-    userlocation:"asdf",
+    userlocation:"",
     followingnum: 0,
     followernum: 0,
+    sharelocation: 1,
   },
   
   onLaunch: function () {
