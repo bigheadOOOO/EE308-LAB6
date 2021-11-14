782ed7c58dc44c66c983d69454dd680dc1145c7f
bigheadOOOO <90515693+bigheadOOOO@users.noreply.github.com>
2021-11-14 17:35:26 +0800

    Add files via upload

--- miniprogram/miniprogram/app.js
+++ miniprogram/miniprogram/app.js
@@ -6,12 +6,15 @@ App({
   globalData:{
     username:"USER",
     userimage:"https://s3.bmp.ovh/imgs/2021/10/77a830cbddfff9a8.png",
-    userlocation:"asdf"
+    userlocation:"asdf",
+    followingnum: 0,
+    followernum: 0,
   },
   
   onLaunch: function () {
+    wx.cloud.init({
+      traceUser: true,
 
-
-    this.globalData = {}
+  })
   }
 })
