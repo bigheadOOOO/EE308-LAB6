57bfe2114c4441ff1fc8cf8c3c44faf9d8b851e5
bigheadOOOO <90515693+bigheadOOOO@users.noreply.github.com>
2021-11-17 11:07:32 +0800

    Add files via upload

--- miniprogram/miniprogram/pages/1_login/login.js
+++ miniprogram/miniprogram/pages/1_login/login.js
@@ -36,73 +36,75 @@ Page({
       allusers.get({
         success:function(re){
           if(re){
-            console.log(re)
-            for(i = 0; i < re.data.length; i++){
-              if ((n == re.data[i])&&(im == re.data[i])){
+            console.log(re.data)
+            console.log(n)
+            for(var i = 0; i < re.data.length; i++){
+              if ((n == re.data[i].name)&&(im == re.data[i].img)){
                 console.log("cvbn");
-                app.globalData.myprovince = re[i].myprovince
+                app.globalData.myprovince = re.data[i].myprovince
                 that.setData({
                   exist: 1
                 })
                 break;
               }
             }
+          }console.log(that.data.exist)
+          if((that.data.ok == true)&&(that.data.exist == 0)){ // 没有在数据库中，就加入数据库
+            //获取当前位置
+            wx.getLocation({
+              success: (res) => {
+                console.log(res)
+                that.setData({
+                  userlatitude: res.latitude,
+                  userlongitude: res.longitude
+                })
+                // 判断城市
+                if((that.data.userlatitude > 20.30) && (that.data.userlatitude < 28.22) && (that.data.userlongitude > 115.50) && (that.data.userlatitude < 120.40)){
+                  that.setData({
+                    province: "Fujian"
+                  })
+                  
+                }else if((that.data.userlatitude > 39) && (that.data.userlatitude < 40) && (that.data.userlongitude > 115.50) && (that.data.userlatitude < 117)){
+                  that.setData({
+                    province: "Beijing"
+                  })
+                  app.globalData.myprovince = "Beijing"
+                  
+                }else if((that.data.userlatitude > 38.33) && (that.data.userlatitude < 40.16) && (that.data.userlongitude > 116.42) && (that.data.userlatitude < 118.20)){
+                  that.setData({
+                    province: "Tianjing"
+                  })
+                  
+                }else if((that.data.userlatitude > 31.1) && (that.data.userlatitude < 31.2) && (that.data.userlongitude > 121.25) && (that.data.userlatitude < 212.35)){
+                  that.setData({
+                    province: "Shanghai"
+                  })
+                  
+                }else if((that.data.userlatitude > 105.17) && (that.data.userlatitude < 110.11) && (that.data.userlongitude > 115.50) && (that.data.userlatitude < 117)){
+                  that.setData({
+                    province: "Chongqing"
+                  })
+                  
+                }else if((that.data.userlatitude > 118.01) && (that.data.userlatitude < 123.1) && (that.data.userlongitude > 27.02) && (that.data.userlatitude < 31.11)){
+                  that.setData({
+                    province: "Zhejiang"
+                  })
+                }app.globalData.myprovince = that.data.province
+                console.log(app.globalData.myprovince)
+                allusers.add({
+                  data:{
+                    name: app.globalData.username,
+                    img: app.globalData.userimage,
+                    myprovince: that.data.province,
+                    mycity: ""
+                  }
+                });
+              }
+            })
           }
         }
       })
-      if((that.data.ok == true)&&(that.data.exist == 0)){ // 没有在数据库中，就加入数据库
-        //获取当前位置
-        wx.getLocation({
-          success: (res) => {
-            console.log(res)
-            that.setData({
-              userlatitude: res.latitude,
-              userlongitude: res.longitude
-            })
-            // 判断城市
-            if((that.data.userlatitude > 20.30) && (that.data.userlatitude < 28.22) && (that.data.userlongitude > 115.50) && (that.data.userlatitude < 120.40)){
-              that.setData({
-                province: "Fujian"
-              })
-              
-            }else if((that.data.userlatitude > 39) && (that.data.userlatitude < 40) && (that.data.userlongitude > 115.50) && (that.data.userlatitude < 117)){
-              that.setData({
-                province: "Beijing"
-              })
-              app.globalData.myprovince = "Beijing"
-              
-            }else if((that.data.userlatitude > 38.33) && (that.data.userlatitude < 40.16) && (that.data.userlongitude > 116.42) && (that.data.userlatitude < 118.20)){
-              that.setData({
-                province: "Tianjing"
-              })
-              
-            }else if((that.data.userlatitude > 31.1) && (that.data.userlatitude < 31.2) && (that.data.userlongitude > 121.25) && (that.data.userlatitude < 212.35)){
-              that.setData({
-                province: "Shanghai"
-              })
-              
-            }else if((that.data.userlatitude > 105.17) && (that.data.userlatitude < 110.11) && (that.data.userlongitude > 115.50) && (that.data.userlatitude < 117)){
-              that.setData({
-                province: "Chongqing"
-              })
-              
-            }else if((that.data.userlatitude > 118.01) && (that.data.userlatitude < 123.1) && (that.data.userlongitude > 27.02) && (that.data.userlatitude < 31.11)){
-              that.setData({
-                province: "Zhejiang"
-              })
-            }app.globalData.myprovince = that.data.province
-            console.log(app.globalData.myprovince)
-            allusers.add({
-              data:{
-                name: app.globalData.username,
-                img: app.globalData.userimage,
-                myprovince: that.data.province,
-                mycity: ""
-              }
-            });
-          }
-        })
-      }
+      
       wx.switchTab({
         url: '../2_1home/home'})
     }
